1a17989700f155a980f3083960cfeabf88639c8a
Smaine Kahlouch <smainklh@gmail.com>
2024-06-30 09:51:24 +0200

    daggerize: multi-stage build

--- dagger/main.go
+++ dagger/main.go
@@ -3,32 +3,97 @@ package main
 import (
 	"context"
 	"fmt"
+	"strconv"
+)
+
+const (
+	alpineImage = "alpine:3.20.1"
+	golangImage = "golang:1.22"
 )
 
 type GolangHelloworld struct{}
 
+// Build and publish Docker container
+func (m *GolangHelloworld) Build(ctx context.Context, source *Directory) (*Container, error) {
+	// build the binary
+	builder := dag.Container().
+		From(golangImage).
+		WithDirectory("/src", source).
+		WithWorkdir("/src").
+		WithEnvVariable("CGO_ENABLED", "0").
+		WithExec([]string{"go", "build", "-o", "helloworld", "cmd/helloworld/main.go"})
+
+	// publish binary on alpine base
+	targetImage := dag.Container().
+		From(alpineImage).
+		WithFile("/bin/helloworld", builder.File("/src/helloworld")).
+		WithEntrypoint([]string{"/bin/helloworld"})
+
+	return targetImage, nil
+}
+
 // Test runs go test on the provided source directory
 func (m *GolangHelloworld) Test(ctx context.Context, source *Directory) (string, error) {
-	ctr := dag.Container().From("golang:1.22")
+	ctr := dag.Container().From(golangImage)
 	return ctr.
-		WithWorkdir("/mnt").
-		WithMountedDirectory("/mnt", source).
+		WithWorkdir("/src").
+		WithMountedDirectory("/src", source).
 		WithExec([]string{"go", "test", "./..."}).
 		Stdout(ctx)
 }
 
 // Serve builds and start a server for the provided source directory and a postgres database
-func (m *GolangHelloworld) Serve(ctx context.Context, source *Directory, pgUser *Secret, pgPass *Secret, initScriptDir *Directory) (*Container, error) {
+func (m *GolangHelloworld) Serve(
+	ctx context.Context,
+
+	// The source directory of the application
+	// +optional
+	source *Directory,
+
+	// The database name to use (if not set "wordsdb" is used)
+	// +optional
+	// +default="wordsdb"
+	dbName string,
+
+	// The postgres user secret
+	pgUser *Secret,
+
+	// The postgres password secret
+	pgPass *Secret,
+
+	// The pgPort to use (if not set "5432" is used)
+	// +optional
+	// +default="5432"
+	pgPort string,
+
+	// The directory containing the init script for the postgres database
+	// +optional
+	initScriptDir *Directory,
+
+	// cache is a flag to enable caching of the postgres container
+	// +optional
+	// +default=false
+	cache bool,
+
+) (*Container, error) {
+
+	if initScriptDir == nil {
+		initScriptDir = source.Directory("./scripts")
+	}
 
 	opts := PostgresOpts{
-		DbName:     "wordsdb",
-		Cache:      false,
+		DbName:     dbName,
+		Cache:      cache,
 		Version:    "13",
 		ConfigFile: nil,
 		InitScript: initScriptDir,
 	}
-
-	pgCtr := dag.Postgres(pgUser, pgPass, 5432, opts).Database().WithMountedDirectory("/docker-entrypoint-initdb.d", initScriptDir)
+	// convert pgPort to int
+	pgPortInt, err := strconv.Atoi(pgPort)
+	if err != nil {
+		return nil, fmt.Errorf("could not convert pgPort to int: %w", err)
+	}
+	pgCtr := dag.Postgres(pgUser, pgPass, pgPortInt, opts).Database()
 
 	pgSvc := pgCtr.AsService()
 
@@ -37,16 +102,17 @@ func (m *GolangHelloworld) Serve(ctx context.Context, source *Directory, pgUser 
 		return nil, fmt.Errorf("could not get postgres hostname: %w", err)
 	}
 
-	ctr := dag.Container().From("golang:1.22")
+	ctr, err := m.Build(ctx, source)
+	if err != nil {
+		return nil, fmt.Errorf("could not build container: %w", err)
+	}
+
 	return ctr.
-		WithWorkdir("/mnt").
-		WithMountedDirectory("/mnt", source).
 		WithSecretVariable("PGPASSWORD", pgPass).
 		WithSecretVariable("PGUSER", pgUser).
 		WithEnvVariable("PGHOST", pgHostname).
 		WithEnvVariable("PGDATABASE", opts.DbName).
-		WithEnvVariable("PGPORT", "5432").
-		WithExec([]string{"go", "run", "cmd/helloworld/main.go"}).
+		WithEnvVariable("PGPORT", pgPort).
 		WithServiceBinding("database", pgSvc).
 		WithExposedPort(8080), nil
 }
